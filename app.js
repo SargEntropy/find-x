ee783ec9d7bf7ae31e092bac61173dd797ab1cef
GERMAN ALEJANDRO SANDOVAL RODRIGUEZ <german.sandoval@ibm.com>
2018-05-29 10:29:11 -0500

    Adding uploading files

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const express = require('express'),
       exphbs = require('express-handlebars'),
       request = require('request'),
       bodyParser = require('body-parser'),
-      // jsonfile = require('jsonfile'),
+      fileUpload = require('express-fileupload'),
+      csv = require('fast-csv'),
       fs = require('fs');
 
 const PORT = process.env.PORT || 5000
@@ -14,6 +15,7 @@ express()
   .use(express.static(__dirname + '/public'))
   .use('/public', express.static(__dirname + '/public'))
   .use(bodyParser.urlencoded({ extended: true }))
+  .use(fileUpload())
   .engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', '.hbs')
@@ -59,6 +61,21 @@ express()
           res.redirect('/');
         }
       }
+    });
+  })
+  .post('/drag', function(req, res) {
+    if (!req.files)
+      return res.status(400).send('No files were uploaded.');
+
+    let data = req.files.data;
+    // Use the mv() method to place the file somewhere on your server
+    data.mv('./public/montreal_2014.csv', function(err) {
+      if (err)
+        return res.status(500).send(err);
+
+      let extData = data.data.toString('utf-8');
+      console.log(extData);
+      res.send('File uploaded!');
     });
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
